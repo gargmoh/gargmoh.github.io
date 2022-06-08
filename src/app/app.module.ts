6b01c5eaf7dc9a9e01a4974be2e98d108feb9af1
gargmoh <kishulehra@gmail.com>
2022-06-07 22:55:40 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogsComponent } from './components/logs/logs.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogFormComponent,
+    NavbarComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
